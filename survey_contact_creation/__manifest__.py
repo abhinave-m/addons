25e05dcd2907143c91cb2194be46103a14986cdd
abhinave-m <m10abhinave@gmail.com>
2025-07-15 09:31:53 +0530

    happy?

--- survey_contact_creation/__manifest__.py
+++ survey_contact_creation/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Survey contact creation",
     'version': '18.0.1.0.0',
     'depends': ['base','survey', 'contacts'],
-    'author': "AMR",
+    'author': "AMRan",
     'category': 'Category',
     'description': """
     Helps in creation of contacts from surveys
